bc2d8a96c77d653c880a57a8cb8c105f90774b6f
Mateo <mateopentito@hotmal.com>
2026-03-09 22:43:29 +0100

    Agregue mas estilos

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const apiKey = 'e9fbb2c4';
 const boton_buscar = document.getElementById("boton-buscar");
 const input_pelicula = document.getElementById("input-pelicula");
 const resultado = document.getElementById("resultado");
-
+const boton_categoria = document.querySelectorAll(".categorias button");
 
 
 
@@ -67,3 +67,55 @@ input_pelicula.addEventListener("keydown", (e) => {
         buscarPeliculas();
     }
 })
+
+
+
+//Categoria
+
+boton_categoria.forEach(boton => {
+    boton.addEventListener("click", () => {
+        const genero = boton.dataset.genero;
+        buscarPeliculasPorGenero(genero);
+    })
+})
+
+async function buscarPeliculasPorGenero(genero) {
+    const cargarDiv = document.createElement("p");
+    cargarDiv.textContent = "Buscando peliculas...";
+    resultado.appendChild(cargarDiv);
+
+    try {
+        const response = await fetch(`https://www.omdbapi.com/?s=${genero}&apikey=e9fbb2c4`);
+        const data = await response.json();
+
+        resultado.removeChild(cargarDiv);
+
+        if (data.Response === "True") {
+            for (const pelicula of data.Search) {
+                const detalleResponse = await fetch(`https://www.omdbapi.com/?i=${pelicula.imdbID}&apikey=e9fbb2c4`);
+                const detalle = await detalleResponse.json();
+
+                if (detalle.Genre.includes(genero)) {
+                    const peliculaDiv = document.createElement("div");
+                    peliculaDiv.classList.add("pelicula");
+
+                    peliculaDiv.innerHTML = `
+                        <h3>${detalle.Title}</h3>
+                        <p>Año: ${detalle.Year}</p>
+                        <p>Genero: ${detalle.Genre}</p>
+                        <p>${detalle.Plot}</p>
+                        <img src="${detalle.Poster !== "N/A" ? detalle.Poster : "https://via.placeholder.com/150"}">
+                    `;
+                    resultado.appendChild(peliculaDiv);
+                } else {
+                    const error = document.createElement("p");
+                    error.textContent = "No se encontraron peliculas."
+                    resultado.appendChild(error);
+                }
+
+            }
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
